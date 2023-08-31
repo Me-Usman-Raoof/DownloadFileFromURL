62c8ca225ede9fc19440b13cdff3c6bd65ff0401
UsmanRaoof <usman.rauf@intellicode.tech>
2023-08-30 23:32:33 -0700

    Basic Changes To the Library

--- app/src/main/java/com/paulfrank/downloadpdf/MainActivity.java
+++ app/src/main/java/com/paulfrank/downloadpdf/MainActivity.java
@@ -25,56 +25,18 @@ import com.paulfrank.makeitdownload.FileDownloaderService;
 import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnDownload;
-    private TextView progressCount;
-
-    //    String pdfurl = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
-    String pdfurl = "https://sc-staging.algoras.com/images/logo.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnDownload = findViewById(R.id.btnDownload);
-        progressCount = findViewById(R.id.progressCount);
 
-        btnDownload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Log.d("FOO", "Button Clicked");
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("Choose Download Method");
-                builder.setNegativeButton("Downlaod Manager", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        downloadPDF(pdfurl);
-                    }
-                });
-                builder.setPositiveButton("Using Browser", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(pdfurl)));
-                    }
-                });
-                builder.show();
-            }
-        });
-
-    }
-
-    private void downloadPDF(String pdfURL) {
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(pdfURL));
-        request.setTitle("Downloading the Report");
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, getFileNameWithExtensionFromURL(pdfURL));
-        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-        long downloadId = manager.enqueue(request);
-    }
-
-    private String getFileNameWithExtensionFromURL(String fileURL) {
-        Uri uri = Uri.parse(fileURL);
-        return uri.getLastPathSegment();
+        FileDownloaderService downloaderService = new FileDownloaderService(
+                MainActivity.this,
+                "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+                "Downloading",
+                1
+        );
+        downloaderService.startDownloading();
     }
 }
--- makeitdownload/src/main/java/com/paulfrank/makeitdownload/FileDownloaderService.java
+++ makeitdownload/src/main/java/com/paulfrank/makeitdownload/FileDownloaderService.java
@@ -4,48 +4,67 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class FileDownloaderService {
     private Context context = null;
     private String fileURL = null;
+    private String notificationTitle = null;
     private Integer notificationVisibility = null;
-
-    private ProgressBar progressBar = null;
 
     public FileDownloaderService(Context context,
                                  String fileURL,
+                                 String notificationTitle,
                                  Integer notificationVisibility) {
         this.context = context;
         this.fileURL = fileURL;
+        this.notificationTitle = notificationTitle;
         this.notificationVisibility = notificationVisibility;
     }
 
     public void startDownloading() {
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(fileURL));
-        request.setTitle("Downloading the Report");
-        request.setNotificationVisibility(notificationVisibility);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, getFileNameWithExtensionFromURL(fileURL));
-        DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        manager.enqueue(request);
+
+        if (isValidate()) {
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(fileURL));
+            request.setTitle("Downloading the Report");
+            request.setNotificationVisibility(notificationVisibility);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, getFileNameWithExtensionFromURL(fileURL));
+            DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            manager.enqueue(request);
+        } else {
+            Log.d("FOO", "Something Went Wrong While Downloading The File");
+        }
+    }
+
+    protected boolean isValidate() {
+        int value = 0;
+        if (context == null) {
+            value = 1;
+            Log.d("FOO", "Context Is Null While Downloading File");
+        } else if (fileURL == null || TextUtils.isEmpty(fileURL)) {
+            value = 1;
+            Log.d("FOO", "File Url Is Null While Downloading File");
+        } else if (notificationTitle == null || TextUtils.isEmpty(notificationTitle)) {
+            notificationTitle = "Downloading File";
+        } else if (notificationVisibility == null) {
+            notificationVisibility = 1;
+        } else {
+            value = 0;
+        }
+        return value == 0;
     }
 
     private String getFileNameWithExtensionFromURL(String fileURL) {
         Uri uri = Uri.parse(fileURL);
         return uri.getLastPathSegment();
-    }
-
-    // Add this method to set the ProgressBar
-    public void setProgressBar(ProgressBar progressBar) {
-        this.progressBar = progressBar;
-    }
-
-    // Update the ProgressBar with the download progress
-    private void updateProgressBar(int progress) {
-        if (progressBar != null) {
-            progressBar.setProgress(progress);
-        }
     }
 }
